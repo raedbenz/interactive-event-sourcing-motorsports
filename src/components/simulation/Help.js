dda93cb79e21628037e92cf98d12d1808b62c4f7
Raed AbuSanad <raed.abusanad@hotmail.com>
2019-01-15 11:04:38 +0000

    Help.js

--- src/components/simulation/Help.js
+++ src/components/simulation/Help.js
@@ -6,7 +6,7 @@ function Help() {
             <li>Click 'Start Run' to start a run</li>
             <li>Each run is consisted of 3 laps; (out-lap, hot-lap, and in-lap)</li>
             <li>Yellow bolts <img src="images/light.svg" height="24px" width="24px" /> are the lap triggers installed on the track</li>
-            <li>An aggregate snapshot is taken every 2 runs</li>
+            <li>An aggregate snapshot is taken every 2 runs; min, max, and mean of all lap times</li>
         </ul>
     );
 }
